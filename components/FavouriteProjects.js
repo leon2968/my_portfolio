afdf2a498a3719541893a7bd7e9253a1d1c292e6
Leon Zheng <39258470+leon2968@users.noreply.github.com>
2022-05-19 23:31:10 -0400

    Update FavouriteProjects.js

--- components/FavouriteProjects.js
+++ components/FavouriteProjects.js
@@ -35,7 +35,7 @@ export default function FavouriteProjects() {
         <div className="grid md:grid-cols-3 gap-8 lg:-mt-8 pb-40">
           {/* Single card */}
           <a
-            href="https://tailwindmasterkit.com"
+            href=""
             className="w-full block col-span-3 shadow-2xl"
           >
             <div className="relative overflow-hidden">
@@ -54,7 +54,7 @@ export default function FavouriteProjects() {
           </a>
           {/* Single card */}
           <a
-            href="https://placeholdertech.in"
+            href=""
             className="w-full block col-span-3  sm:col-span-2 shadow-2xl"
           >
             <div className="relative overflow-hidden">
