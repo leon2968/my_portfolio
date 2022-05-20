0d7b716a9e9a536dc6ed0a80d4c7e864d08f8531
Leon Zheng <39258470+leon2968@users.noreply.github.com>
2022-05-20 17:24:37 -0400

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -4,7 +4,7 @@ import Contact from "../components/Contact";
 
 export default function contact() {
   return (
-    <ContainerBlock>
+    <ContainerBlock title='Leon Zheng - Contact'>
       <Contact />
     </ContainerBlock>
   );
