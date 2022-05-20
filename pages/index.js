f6f1dd758e4a796c335e2e7131ae68c9b04dba3e
Leon Zheng <39258470+leon2968@users.noreply.github.com>
2022-05-19 20:27:06 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import userData from "@constants/data";
 export default function Home({ repositories }) {
   return (
     <ContainerBlock
-      title="Leon Zheng - Developer, Writer, Creator"
+      title="Leon Zheng - Developer, Creator, Trader, Gamer"
       description="This is a template built specifically for my blog - Creating a developer portfolio that gets you a job."
     >
       <Hero />
