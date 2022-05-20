cb8e96b2b87ead4be1c6c5c99756f9b99a9d2338
Leon Zheng <39258470+leon2968@users.noreply.github.com>
2022-05-19 22:06:57 -0400

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -11,24 +11,24 @@ export default function Hero() {
 
       <div className="w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
         <RoughNotationGroup show={true}>
-          <RainbowHighlight color={colors[0]}>
+          <RainbowHighlight color={colors[5]}>
             <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
               Developer.
             </h1>
           </RainbowHighlight>
           <RainbowHighlight color={colors[1]}>
             <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
-              Designer.
+              Trader.
             </h1>
           </RainbowHighlight>
           <RainbowHighlight color={colors[2]}>
             <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
-              Programmer.
+              Leaner.
             </h1>
           </RainbowHighlight>
           <RainbowHighlight color={colors[3]}>
             <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
-              Youtuber.
+              Gamer.
             </h1>
           </RainbowHighlight>
         </RoughNotationGroup>
