fa220d9ce9985ce66017e06c2711d371816ee182
Leon Zheng <39258470+leon2968@users.noreply.github.com>
2022-05-20 18:35:12 -0400

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -111,6 +111,7 @@ export default function Navbar() {
               )}
             </a>
           </Link>
+          <!--
           <Link href="/contact">
             <a
               className={`text-base  ${
@@ -119,7 +120,7 @@ export default function Navbar() {
                   : "text-gray-600 dark:text-gray-300 font-normal "
               }`}
             >
-              Contact
+            //  Contact
               {router.asPath === "/contact" && (
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -128,7 +129,7 @@ export default function Navbar() {
                   fill="currentColor"
                   className="bi bi-arrow-down inline-block h-3 w-3"
                   viewBox="0 0 16 16"
-                >
+                >*/
                   <path
                     fillRule="evenodd"
                     d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
@@ -137,6 +138,7 @@ export default function Navbar() {
               )}
             </a>
           </Link>
+          -->
         </div>
 
         <div className="space-x-4 flex flex-row items-center">
@@ -235,11 +237,13 @@ export default function Navbar() {
             Experience
           </a>
         </Link>
+        <!--
         <Link href="/contact">
           <a className="text-base font-normal text-gray-600 dark:text-gray-300">
             Contact
           </a>
         </Link>
+        -->
       </div>
     </div>
   );
