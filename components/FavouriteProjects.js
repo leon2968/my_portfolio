c7847ee611d58262eb4efc40aa5288bb375df7fe
Leon Zheng <39258470+leon2968@users.noreply.github.com>
2023-02-20 04:58:04 -0500

    Update FavouriteProjects.js

--- components/FavouriteProjects.js
+++ components/FavouriteProjects.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 export default function FavouriteProjects() {
   return (
+    /*
     <div className="bg-[#F1F1F1] -mt-40 dark:bg-gray-900">
       <div className="max-w-6xl mx-auto">
         <header className="flex flex-col md:flex-row justify-between items-center pt-40 mx-10 md:my-20 lg:my-0">
@@ -55,5 +56,6 @@ export default function FavouriteProjects() {
         </div>
       </div>
     </div>
+  */
   );
 }
